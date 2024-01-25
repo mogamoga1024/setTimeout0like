044b54d20a22849adb0ab24ddeff2b8072b903e0
mogamoga <mogamoga.g1thu6@gmail.com>
2024-01-25 10:30:53 +0900

    123

--- main.js
+++ main.js
@@ -4,9 +4,11 @@
 //     domResult.innerText = result0;
 // }
 
+const MAX_NUM = 10000000;
+
 const domResult1 = document.querySelector("#result1");
 let result1 = 0;
-setTimeout(function loop(num = 1, maxNum = 10000000) {
+setTimeout(function loop(num = 1, maxNum = MAX_NUM) {
     result1 += num;
     domResult1.innerText = result1;
     if (num < maxNum) {
@@ -16,10 +18,13 @@ setTimeout(function loop(num = 1, maxNum = 10000000) {
 
 const domResult2 = document.querySelector("#result2");
 let result2 = 0;
-setZeroTimeout(function loop(num = 1, maxNum = 10000000) {
+setZeroTimeout(function loop(num = 1, maxNum = MAX_NUM) {
     result2 += num;
     domResult2.innerText = result2;
     if (num < maxNum) {
         setZeroTimeout(loop)
     }
-}, 0);
+});
+
+
+
